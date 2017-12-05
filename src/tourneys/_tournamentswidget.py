94712367cd9c1e6fec07a7675a19c0878f880698
MathieuB8 <masamune31@hotmail.fr>
2017-12-05 19:33:47 +0100

    WIP

--- src/tourneys/_tournamentswidget.py
+++ src/tourneys/_tournamentswidget.py
@@ -40,29 +40,20 @@ class TournamentsWidget(FormClass, BaseClass):
         self.setupUi(self)
 
         self.client = client
-
-
         self.tourneyList.currentItemChanged.connect(self.itemChanged)
-
-        self.tourneysTab = {}
-        #item = QListWidgetItem("Item ZAWA")
-        self.show_tournaments_informations()
-        # Special stylesheet
         util.THEME.setStyleSheet(self, "tournaments/formatters/style.css")
+        self.show_tournaments_informations()
 
     def itemChanged(self,current,previous):
-        #QtWidgets.QMessageBox.about(self.client, "tesZEA", 'BOOM CLICKED')
-        text=str(current.data(QtCore.Qt.UserRole).name)+'\nDescription : '+str(current.data(QtCore.Qt.UserRole).description)+'\nNumber of participants : '+str(current.data(QtCore.Qt.UserRole).number_of_participants)+'\nStart time : '+str(current.data(QtCore.Qt.UserRole).start_time)+'\nTournament type : '+str(current.data(QtCore.Qt.UserRole).tournament_type)
-
+        text=str(current.data(QtCore.Qt.UserRole).name)+'\n\nDescription : '+str(current.data(QtCore.Qt.UserRole).description)+'\n\nNumber of participants : '+str(current.data(QtCore.Qt.UserRole).number_of_participants)+'\n\nStart time : '+str(current.data(QtCore.Qt.UserRole).start_time)+'\n\nTournament type : '+str(current.data(QtCore.Qt.UserRole).tournament_type)
         self.currentTournamentDetails.setText(str(text))
-
-        url=str(current.data(QtCore.Qt.UserRole).live_image_url)
+        url=str(current.data(QtCore.Qt.UserRole).full_challonge_url+'/module')
+        #url='https://faforever.com/competitive/tournaments'
         self.tournamentsWebView.setUrl(QUrl(url))
 
     def tourneys_general_information_error(self,resp):
         logger.error('tourneys_general_information error : '+str(resp))
         return -5
-
 
     def tourneys_general_information_result(self,response):
         result=[]
@@ -70,32 +61,20 @@ class TournamentsWidget(FormClass, BaseClass):
         logger.info('tourneys_general_information_result api response is : '+str(response))
         for alltournaments in reversed(response):
             if alltournaments.get('tournament')!= None and len(alltournaments['tournament']) >= 1:
-                if 1==1 or alltournaments['tournament']['state']=='complete':
-                    '''myQCustomQWidget = QCustomQWidget()
-                    myQCustomQWidget.setTextUp('zawa')
-                    myQCustomQWidget.setTextDown(alltournaments['tournament']['name']+'\nState:'+alltournaments['tournament']['state']+'\n')
-                    self.tourneyList.addItem(myQCustomQWidget)'''
-                    myTourney = TournamentInfo()
-                    myTourney.name = alltournaments['tournament']['name']
-                    myTourney.state = alltournaments['tournament']['state']
-                    myTourney.number_of_participants = alltournaments['tournament']['participants_count']
-                    myTourney.tournament_type = alltournaments['tournament']['tournament_type']
-                    myTourney.live_image_url = alltournaments['tournament']['live_image_url']
-                    myTourney.start_time = alltournaments['tournament']['start_at']
-                    myTourney.full_challonge_url = alltournaments['tournament']['full_challonge_url']
-                    myTourney.description = alltournaments['tournament']['description']
+                myTourney = TournamentInfo()
+                myTourney.name = alltournaments['tournament']['name']
+                myTourney.state = alltournaments['tournament']['state']
+                myTourney.number_of_participants = alltournaments['tournament']['participants_count']
+                myTourney.tournament_type = alltournaments['tournament']['tournament_type']
+                myTourney.live_image_url = alltournaments['tournament']['live_image_url']
+                myTourney.start_time = alltournaments['tournament']['start_at']
+                myTourney.full_challonge_url = alltournaments['tournament']['full_challonge_url']
+                myTourney.description = alltournaments['tournament']['description']
 
-                    item = QtWidgets.QListWidgetItem()
-                    item.setText(alltournaments['tournament']['name']+'\nState : '+alltournaments['tournament']['state']+'\n\n')
-                    item.setData(QtCore.Qt.UserRole, myTourney)
-                    self.tourneyList.addItem(item)
-
-
-
-
-                    #item = QtWidgets.QListWidgetItem('A Text-Only Item')
-                    #self.tourneyList.addItem(alltournaments['tournament']['name']+'\nState : '+alltournaments['tournament']['state']+'\n\n')
-
+                item = QtWidgets.QListWidgetItem()
+                item.setText(myTourney.name+'\nState : '+myTourney.state+'\n\n')
+                item.setData(QtCore.Qt.UserRole, myTourney)
+                self.tourneyList.addItem(item)
         self.tourneyList.show()
 
     def show_tournaments_informations(self):
