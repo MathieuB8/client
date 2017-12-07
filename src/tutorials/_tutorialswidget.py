e01bb84a21f70a8b24e50252b9cc2f59bec512e7
MathieuB8 <masamune31@hotmail.fr>
2017-12-07 22:52:37 +0100

    wip

--- src/tutorials/_tutorialswidget.py
+++ src/tutorials/_tutorialswidget.py
@@ -16,6 +16,7 @@ class TutorialsWidget(FormClass, BaseClass):
         self.setupUi(self)
         self.client = client
         util.THEME.setStyleSheet(self, "tutorials/formatters/style.css")
+        #util.THEME.setStyleSheet(self, "tournaments/formatters/style.css")
 
         self.label_image_video1.setPixmap(QtGui.QPixmap('res/tutorials/TopTipsFAF.png'))
         self.label_text_video1.insertHtml('''<center>
@@ -29,7 +30,7 @@ class TutorialsWidget(FormClass, BaseClass):
         <a href='https://www.youtube.com/watch?v=tqCs0WVGT_I'><span style="color:#009933;">Link to video</span></a>
         </center>''')
 
-        self.tutorial_text_browser.insertHtml('''<center>
+        self.tutorial_main_text_browser.insertHtml('''<center>
         Welcome to forged alliance forever ! If you want ever more information about this game you can check these tutorials in order to improve :<br>
         <a href='https://wiki.faforever.com/index.php?title=Main_Page'><span style="color:#009933;">Link to wiki</span></a><br>
         <br>
@@ -42,17 +43,15 @@ class TutorialsWidget(FormClass, BaseClass):
         Some trainers are also available to help you improve on the game, so don't hesitate to contact them at all !<br>
         You can ask for one in #aeolus or  in #newbie channel or find a name to contact on this topic :<br>
         <a href ="http://forums.faforever.com/viewtopic.php?f=2&t=1614"><span style="color:#009933;">Your Personal Trainer Team</span></a><br>
-        <span style="color:#009933;">(todo, not working)Ask if a personal trainer is available</span>
+        <span style="color:#009933;">(todo, not working)Call for a personal trainer</span>
         </center>''')
 
-        '''Here are some challenges if you want to improve on the game too :<br>
-        todo<br>
-        <br>
-        Here are some training maps (made by speed2) :<br>
-        todo<br>
-        <br>
-        Achievements :<br>
-        (todo, just text right now)<br>
+        self.tutorial_tutorialsmaps_text_browser.insertHtml('''<center>
+        (todo)Here you will find some scenario which come from speed2 idea
+        </center>''')
+
+        self.tutorial_achievements_text_browser.insertHtml('''<center>
+        (todo)Achievements :<br>
         [] : Play 10 different maps on custom games<br>
         [] : Get trained by a trainer<br>
         [] : Reach 600 global rating<br>
@@ -60,6 +59,8 @@ class TutorialsWidget(FormClass, BaseClass):
         [] : Reach 500 ladder rating<br>
         [] : Play 30 different maps on custom games<br>
         [] : Reach 1000 global rating<br>
-        [] : Reach 1000 ladder rating<br>'''
+        [] : Reach 1000 ladder rating<br>
+        </center>''')
+
 
         logger.info("Tutorials instantiated.")
